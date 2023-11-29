202fa606f27959d1afe023f2d256bae808439fb7
Benjamin Moore <benjaminjm@gmail.com>
2023-11-28 20:23:31 -0800

    Add empty solution files

---
+++ src/day02.rs
@@ -0,0 +1,5 @@
+use std::error::Error;
+
+pub fn solve(input: Vec<String>) -> Result<(String, String), Box<dyn Error>> {
+    Ok(("".to_string(), "".to_string()))
+}
---
+++ src/day17.rs
@@ -0,0 +1,5 @@
+use std::error::Error;
+
+fn solve(input: Vec<String>) -> Result<(String, String), Box<dyn Error>> {
+    Ok(("".to_string(), "".to_string()))
+}
