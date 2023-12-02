012f0c6e91af9431c29643cead5d4813aa6f7aa1
Benjamin Moore <benjaminjm@gmail.com>
2023-12-01 16:15:05 -0800

    Implement day1 part 2

--- src/day01.rs
+++ src/day01.rs
@@ -44,13 +44,129 @@ enum State {
 impl StateMachine {
     fn match_digit(&mut self, c: char) -> Option<u32> {
         self.state = match &self.state {
+            State::One1 => match c {
+                'n' => State::One2,
+                _ => State::Init,
+            },
             State::One2 => match c {
-                'n' => State::One3,
+                'i' => State::Nine2,
+                'e' => State::One3,
+                _ => State::Init,
+            },
+            State::One3 => match c {
+                'i' => State::Eight2,
                 _ => State::Init,
             },
             State::InitT => match c {
                 'w' => State::Two2,
                 'h' => State::Three2,
+                _ => State::Init,
+            },
+            State::Two2 => match c {
+                'o' => State::Two3,
+                _ => State::Init,
+            },
+            State::Two3 => match c {
+                'n' => State::One2,
+                _ => State::Init,
+            },
+            State::Three2 => match c {
+                'r' => State::Three3,
+                _ => State::Init,
+            },
+            State::Three3 => match c {
+                'e' => State::Three4,
+                _ => State::Init,
+            },
+            State::Three4 => match c {
+                'e' => State::Three5,
+                'i' => State::Eight2,
+                _ => State::Init,
+            },
+            State::Three5 => match c {
+                'i' => State::Eight2,
+                _ => State::Init,
+            },
+            State::InitF => match c {
+                'o' => State::Four2,
+                'i' => State::Five2,
+                _ => State::Init,
+            },
+            State::Four2 => match c {
+                'n' => State::One2,
+                'u' => State::Four3,
+                _ => State::Init,
+            },
+            State::Four3 => match c {
+                'r' => State::Four4,
+                _ => State::Init,
+            },
+            State::Five2 => match c {
+                'v' => State::Five3,
+                _ => State::Init,
+            },
+            State::Five3 => match c {
+                'e' => State::Five4,
+                _ => State::Init,
+            },
+            State::InitS => match c {
+                'i' => State::Six2,
+                'e' => State::Seven2,
+                _ => State::Init,
+            },
+            State::Six2 => match c {
+                'x' => State::Six3,
+                _ => State::Init,
+            },
+            State::Seven2 => match c {
+                'i' => State::Eight2,
+                'v' => State::Seven3,
+                _ => State::Init,
+            },
+            State::Seven3 => match c {
+                'e' => State::Seven4,
+                _ => State::Init,
+            },
+            State::Seven4 => match c {
+                'i' => State::Eight2,
+                'n' => State::Seven5,
+                _ => State::Init,
+            },
+            State::Eight1 => match c {
+                'i' => State::Eight2,
+                _ => State::Init,
+            },
+            State::Eight2 => match c {
+                'g' => State::Eight3,
+                _ => State::Init,
+            },
+            State::Eight3 => match c {
+                'h' => State::Eight4,
+                _ => State::Init,
+            },
+            State::Eight4 => match c {
+                't' => State::Eight5,
+                _ => State::Init,
+            },
+            State::Eight5 => match c {
+                'w' => State::Two2,
+                'h' => State::Three3,
+                _ => State::Init,
+            },
+            State::Nine1 => match c {
+                'i' => State::Nine2,
+                _ => State::Init,
+            },
+            State::Nine2 => match c {
+                'n' => State::Nine3,
+                _ => State::Init,
+            },
+            State::Nine3 => match c {
+                'e' => State::Nine4,
+                _ => State::Init,
+            },
+            State::Nine4 => match c {
+                'i' => State::Eight2,
                 _ => State::Init,
             },
             _ => State::Init,
@@ -70,7 +186,7 @@ impl StateMachine {
 
         match &self.state {
             State::One3 => Some(1),
-            State::Two2 => Some(2),
+            State::Two3 => Some(2),
             State::Three5 => Some(3),
             State::Four4 => Some(4),
             State::Five4 => Some(5),
@@ -83,30 +199,114 @@ impl StateMachine {
     }
 }
 
-//one
-//two
-//three
-//four
-//five
-//six
-//seven
-//eight
-//nine
-
 pub fn solve(input: Vec<String>) -> Result<(String, String), Box<dyn Error>> {
+    let words = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
     let mut part1 = 0;
+    let mut part2 = 0;
+    let mut linect = 1;
+
     for line in input {
-        let mut first_digit = 0;
-        let mut last_digit = 0;
+        let mut first_digit_one = 0;
+        let mut last_digit_one = 0;
+        let mut first_digit_two = 0;
+        let mut last_digit_two = 0;
+        let mut state_machine = StateMachine { state: State::Init };
         for char in line.chars() {
-            if char.is_ascii_digit() {
-                if first_digit == 0 {
-                    first_digit = char.to_digit(10).unwrap();
+            let next_digit_part_one = if char.is_ascii_digit() {
+                Some(char.to_digit(10).unwrap())
+            } else {
+                None
+            };
+
+            let next_digit_part_two = state_machine.match_digit(char);
+
+            match next_digit_part_one {
+                Some(d) => {
+                    if first_digit_one == 0 {
+                        first_digit_one = d
+                    };
+                    if first_digit_two == 0 {
+                        first_digit_two = d
+                    };
+                    last_digit_one = d;
+                    last_digit_two = d;
                 }
-                last_digit = char.to_digit(10).unwrap();
+                None => (),
+            }
+            match next_digit_part_two {
+                Some(d) => {
+                    if first_digit_two == 0 {
+                        first_digit_two = d
+                    };
+                    last_digit_two = d;
+                }
+                None => (),
             }
         }
-        part1 += first_digit * 10 + last_digit;
+
+        // Some code that roughly implements the "naive" solution, which I used for debugging my state machine:
+        /*
+        let first_digit_validation = words
+            .iter()
+            .filter_map(|s| {
+                if let Some(d) = line.find(s) {
+                    Some((d, s))
+                } else {
+                    None
+                }
+            })
+            .min_by(|x, y| x.0.cmp(&y.0))
+            .unwrap_or((0, &""))
+            .1;
+        let first_digit_validation = match first_digit_validation {
+            &"one" => 1,
+            &"two" => 2,
+            &"three" => 3,
+            &"four" => 4,
+            &"five" => 5,
+            &"six" => 6,
+            &"seven" => 7,
+            &"eight" => 8,
+            &"nine" => 9,
+            _ => 0,
+        };
+        if first_digit_validation != first_digit_two {
+            println!("{linect}: {first_digit_two} {first_digit_validation}");
+        }
+        let last_digit_validation = words
+            .iter()
+            .filter_map(|s| {
+                if let Some(d) = line.rfind(s) {
+                    Some((d, s))
+                } else {
+                    None
+                }
+            })
+            .max_by(|x, y| x.0.cmp(&y.0))
+            .unwrap_or((0, &""))
+            .1;
+        let last_digit_validation = match last_digit_validation {
+            &"one" => 1,
+            &"two" => 2,
+            &"three" => 3,
+            &"four" => 4,
+            &"five" => 5,
+            &"six" => 6,
+            &"seven" => 7,
+            &"eight" => 8,
+            &"nine" => 9,
+            _ => 0,
+        };
+        if last_digit_validation != last_digit_two {
+            println!("{linect}: {last_digit_two} {last_digit_validation}");
+        }
+        */
+
+        part1 += first_digit_one * 10 + last_digit_one;
+        part2 += first_digit_two * 10 + last_digit_two;
+        linect += 1;
     }
-    Ok((part1.to_string(), "".to_string()))
+    Ok((part1.to_string(), part2.to_string()))
 }
